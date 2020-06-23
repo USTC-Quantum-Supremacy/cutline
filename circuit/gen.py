a7e66b85517aee5fbe15fda9304bca5f28796d2c
YouWei Zhao <zhao.uv@gmail.com>
2020-06-23 17:28:32 +0800

    u

--- circuit/gen.py
+++ circuit/gen.py
@@ -4,10 +4,15 @@ q=[37,40,45,50,55,60,66]
 dd=[4,5,6,7,8]
 def gen(n,d):
     return f'node /home/user/zhaouv/cutline/generateCircuit.js {d} {n} EFGH circuit/sycamore{n}_{d}_EFGH.txt'
+def gen2(n,d):
+    return f'node /home/user/zhaouv/cutline/generateCircuit.js {d} {n} IJCDCDIJ circuit/sycamore{n}_{d}_IJCD.txt'
 
 if __name__ == "__main__":
     for n in q:
         for d in dd:
             cmd=gen(n,d)
+            ss=os.popen(cmd)
+            ret=ss.read()
+            cmd=gen2(n,d)
             ss=os.popen(cmd)
             ret=ss.read()
