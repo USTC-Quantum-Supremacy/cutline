19089556b0790d05d2289d02310f3aeebd1af02f
YouWei Zhao <zhao.uv@gmail.com>
2020-07-01 12:52:22 +0800

    update blocks

--- blockly.js
+++ blockly.js
@@ -42,7 +42,7 @@ var toolboxObj = {
   "statement" : [
     // 所有语句块
     CutlineInputBlocks["markQi"].xmlText(),
-    CutlineInputBlocks["orderlist"].xmlText(['[60,61,62,63,64,65,54,55,56,57,58,59,48,49,50,51,52,53,42,43,44,45,46,47,36,37,38,39,40,41,30,31,32,33,34,35,24,25,26,27,28,29,18,19,20,21,22,23,12,13,14,15,16,17,6,7,8,9,10,11,0,1,2,3,4,5]']),
+    CutlineInputBlocks["orderlist"].xmlText(['[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65]']),
     CutlineInputBlocks["orderlist"].xmlText(),
     CutlineInputFunctions.parser.parse({"type":"patternA","pattern":"I","color":"#ff9900"}),
     CutlineInputFunctions.parser.parse({"type":"patternA","pattern":"J","color":"#3333ff"}),
