b9406a5f293266de687cb32179fd00dc97bf357f
YouWei Zhao <zhao.uv@gmail.com>
2020-07-03 17:37:31 +0800

    allcircles tasks

--- check18.js
+++ check18.js
@@ -5,24 +5,101 @@ const execSync = require('child_process').execSync
 
 const fs = require('fs')
 
-let input=JSON.parse(fs.readFileSync('in/check18.json',{encoding:'utf-8'}))
+let inputTPL=JSON.parse(fs.readFileSync('in/check18.json',{encoding:'utf-8'}))
 let sd=new StructDataClass();
 
-let balancedRange=14
-let searchPattern='012323010123230121'
-searchPattern='012323010123230103'
 
+let tasks=[
+    {xsize:"11",balancedRange:6,searchPattern='012323010123'},
+    {xsize:"11",balancedRange:10,searchPattern='012323010123'},
+    {xsize:"11",balancedRange:20,searchPattern='012323010123'},
+    {xsize:"11",balancedRange:6,searchPattern='01232301012323'},
+    {xsize:"11",balancedRange:10,searchPattern='01232301012323'},
+    {xsize:"11",balancedRange:20,searchPattern='01232301012323'},
+    {xsize:"11",balancedRange:6,searchPattern='01232301012321'},
+    {xsize:"11",balancedRange:10,searchPattern='01232301012321'},
+    {xsize:"11",balancedRange:20,searchPattern='01232301012321'},
+    {xsize:"11",balancedRange:6,searchPattern='01232301012303'},
+    {xsize:"11",balancedRange:10,searchPattern='01232301012303'},
+    {xsize:"11",balancedRange:20,searchPattern='01232301012303'},
+    {xsize:"11",balancedRange:6,searchPattern='01232301012301'},
+    {xsize:"11",balancedRange:10,searchPattern='01232301012301'},
+    {xsize:"11",balancedRange:20,searchPattern='01232301012301'},
+    {xsize:"11",balancedRange:6,searchPattern='0123230101232301'},
+    {xsize:"11",balancedRange:10,searchPattern='0123230101232301'},
+    {xsize:"11",balancedRange:20,searchPattern='0123230101232301'},
+    {xsize:"11",balancedRange:6,searchPattern='012323010123230101'},
+    {xsize:"11",balancedRange:10,searchPattern='012323010123230101'},
+    {xsize:"11",balancedRange:20,searchPattern='012323010123230101'},
+    {xsize:"11",balancedRange:6,searchPattern='012323010123230103'},
+    {xsize:"11",balancedRange:10,searchPattern='012323010123230103'},
+    {xsize:"11",balancedRange:20,searchPattern='012323010123230103'},
+    {xsize:"11",balancedRange:6,searchPattern='012323010123230121'},
+    {xsize:"11",balancedRange:10,searchPattern='012323010123230121'},
+    {xsize:"11",balancedRange:20,searchPattern='012323010123230121'},
+    {xsize:"11",balancedRange:6,searchPattern='012323010123230123'},
+    {xsize:"11",balancedRange:10,searchPattern='012323010123230123'},
+    {xsize:"11",balancedRange:20,searchPattern='012323010123230123'},
+    {xsize:"11",balancedRange:6,searchPattern='01232301012323010123'},
+    {xsize:"11",balancedRange:10,searchPattern='01232301012323010123'},
+    {xsize:"11",balancedRange:20,searchPattern='01232301012323010123'},
+    {xsize:"12",balancedRange:6,searchPattern='012323010123'},
+    {xsize:"12",balancedRange:10,searchPattern='012323010123'},
+    {xsize:"12",balancedRange:14,searchPattern='012323010123'},
+    {xsize:"12",balancedRange:6,searchPattern='01232301012323'},
+    {xsize:"12",balancedRange:10,searchPattern='01232301012323'},
+    {xsize:"12",balancedRange:14,searchPattern='01232301012323'},
+    {xsize:"12",balancedRange:6,searchPattern='01232301012321'},
+    {xsize:"12",balancedRange:10,searchPattern='01232301012321'},
+    {xsize:"12",balancedRange:14,searchPattern='01232301012321'},
+    {xsize:"12",balancedRange:6,searchPattern='01232301012303'},
+    {xsize:"12",balancedRange:10,searchPattern='01232301012303'},
+    {xsize:"12",balancedRange:14,searchPattern='01232301012303'},
+    {xsize:"12",balancedRange:6,searchPattern='01232301012301'},
+    {xsize:"12",balancedRange:10,searchPattern='01232301012301'},
+    {xsize:"12",balancedRange:14,searchPattern='01232301012301'},
+    {xsize:"12",balancedRange:6,searchPattern='0123230101232301'},
+    {xsize:"12",balancedRange:10,searchPattern='0123230101232301'},
+    {xsize:"12",balancedRange:14,searchPattern='0123230101232301'},
+    {xsize:"12",balancedRange:6,searchPattern='012323010123230101'},
+    {xsize:"12",balancedRange:10,searchPattern='012323010123230101'},
+    {xsize:"12",balancedRange:14,searchPattern='012323010123230101'},
+    {xsize:"12",balancedRange:6,searchPattern='012323010123230103'},
+    {xsize:"12",balancedRange:10,searchPattern='012323010123230103'},
+    {xsize:"12",balancedRange:14,searchPattern='012323010123230103'},
+    {xsize:"12",balancedRange:6,searchPattern='012323010123230121'},
+    {xsize:"12",balancedRange:10,searchPattern='012323010123230121'},
+    {xsize:"12",balancedRange:14,searchPattern='012323010123230121'},
+    {xsize:"12",balancedRange:6,searchPattern='012323010123230123'},
+    {xsize:"12",balancedRange:10,searchPattern='012323010123230123'},
+    {xsize:"12",balancedRange:14,searchPattern='012323010123230123'},
+    {xsize:"12",balancedRange:6,searchPattern='01232301012323010123'},
+    {xsize:"12",balancedRange:10,searchPattern='01232301012323010123'},
+    {xsize:"12",balancedRange:14,searchPattern='01232301012323010123'},
+]
+tasks.forEach((v,i)=>v.screenName='sc'+(i+1))
 
-input.generatingCircuit[0].pattern=Array.from(searchPattern).map(v=>('IJKL')[v]).join('')
-input.depth=searchPattern.length+''
-input.part1='[]'
-input.balancedRange=balancedRange
-input.searchPattern=searchPattern
+const delay = ms => new Promise(res => setTimeout(res, ms));
+let dotask = async (tasks)=>{
+    for (const task of data) {
+        console.log(JSON.stringify(task))
 
-// console.log(JSON.stringify(input,null,4))
+        let input= JSON.parse(JSON.stringify(inputTPL))
+        let {xsize,balancedRange,searchPattern,screenName}=task
 
-fs.writeFileSync('in/allcircles.json',JSON.stringify(input,null,4),{encoding:'utf-8'})
+        input.generatingCircuit[0].pattern=Array.from(searchPattern).map(v=>('IJKL')[v]).join('')
+        input.depth=searchPattern.length+''
+        input.part1='[]'
+        input.balancedRange=balancedRange
+        input.searchPattern=searchPattern
 
+        fs.writeFileSync('in/allcircles.json',JSON.stringify(input,null,4),{encoding:'utf-8'})
+        await delay(50)
+        execSync(`screen -dmS ${screenName} -L -Logfile output/${screenName}.log node allcircles.js`)
+        await delay(50)
+    }
+}
+dotask(tasks)
 /* 
 screen -dmS abc -L -Logfile output/abc.log node allcircles.js
 */
