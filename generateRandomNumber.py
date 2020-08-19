5f88bb72558a2be77e62d29141b613f0c4e77832
YouWei Zhao <zhao.uv@gmail.com>
2020-08-19 13:11:09 +0800

    update random

--- generateRandomNumber.py
+++ generateRandomNumber.py
@@ -1,8 +1,14 @@
 import numpy as np
 import json
 seed=13874234
-np.random.seed(seed)
-randstr=json.dumps({"seed":seed,"list":np.random.random(10000).tolist()})
+seeds=[
+    13874234,10285102,22886724,92997209,18392462,
+    58869319,80531470,53535483,43935200,23239930]
+randobj={"seed":str(seed),"list":[1,2,3],"seeds":[str(i) for i in seeds]}
+for seed in seeds:
+    np.random.seed(seed)
+    randobj[seed]=np.random.random(10000).tolist()
+randstr=json.dumps(randobj)
 with open('random_.js') as fid:
     sourcestr=fid.read()
 with open('random.js','w') as fid:
