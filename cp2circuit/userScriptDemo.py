f63e613f46db68f71fc97b5639268a43ba6053b4
YouWei Zhao <zhao.uv@gmail.com>
2020-09-16 17:07:22 +0800

    format

--- cp2circuit/userScriptDemo.py
+++ cp2circuit/userScriptDemo.py
@@ -54,18 +54,18 @@ qsti.createTasks(mc.getAllTask())
 # %% [markdown]
 # # 任务
 # 以下内容有一些`taskIndex`应该要换成`taskID`
-# 
+#
 # ## 进行一个任务
 
-taskIndex = qsti.getNewTaskIndex() # 获取一个新待进行的任务(未进行过或者已有都是失败状态)
+taskIndex = qsti.getNewTaskIndex()  # 获取一个新待进行的任务(未进行过或者已有都是失败状态)
 
-qsti.createTaskByIndex(taskIndex) # 会raise一个错误, 如果有进行中的任务
+qsti.createTaskByIndex(taskIndex)  # 会raise一个错误, 如果有进行中的任务
 task = mc.getTask(taskIndex)
 
 # %% [markdown]
 # ## 继续未完成的任务
 
-taskIndex = qsti.getCurrentTaskIndex() # 获取进行中的任务
+taskIndex = qsti.getCurrentTaskIndex()  # 获取进行中的任务
 isCompleted = qsti.checkCompleted(taskIndex)
 if isCompleted:
     # 此时应当使用上一段脚本的开新的任务
@@ -75,8 +75,8 @@ task = mc.getTask(taskIndex)
 # %% [markdown]
 # ## 把一个任务设置为失败
 
-qsti.failTask(reason='...') # 把进行中的任务设置为失败
-qsti.failTask(taskIndex,reason='...') # 指定一个任务设为失败
+qsti.failTask(reason='...')  # 把进行中的任务设置为失败
+qsti.failTask(taskIndex, reason='...')  # 指定一个任务设为失败
 
 
 # %% [markdown]
@@ -84,29 +84,29 @@ qsti.failTask(taskIndex,reason='...') # 指定一个任务设为失败
 # ## 运行指令集
 mc.setFSIMTime(123)
 for instruction in task:
-    qsti.join() # 等待上一个指令运行完, 或者提供异步形式的api
-    iType=instruction['type']
-    if iType=='circuit':
-        circuit=mc.getCircuit(instruction['circuitIndex'])
-        count=instruction['count']
-        index=instruction['index']
-        qsti.runCircuit(index,circuit,count) # 提交式的
-    if iType=='calibration':
+    qsti.join()  # 等待上一个指令运行完, 或者提供异步形式的api
+    iType = instruction['type']
+    if iType == 'circuit':
+        circuit = mc.getCircuit(instruction['circuitIndex'])
+        count = instruction['count']
+        index = instruction['index']
+        qsti.runCircuit(index, circuit, count)  # 提交式的
+    if iType == 'calibration':
         pass
-    if iType=='check':
-        circuit=mc.getCircuit(instruction['checkCircuit'])
-        count=instruction['count']
-        index=instruction['index']
-        checkBasis=qsti.runCheck(index,circuit,count) # 同步式的获取结果, check线路能很快跑完
-        mc.submitCheck(instruction,checkBasis)
+    if iType == 'check':
+        circuit = mc.getCircuit(instruction['checkCircuit'])
+        count = instruction['count']
+        index = instruction['index']
+        # 同步式的获取结果, check线路能很快跑完
+        checkBasis = qsti.runCheck(index, circuit, count)
+        mc.submitCheck(instruction, checkBasis)
 
 qsti.join()
 # 全部结束后
-taskBasis=qsti.queryTaskResult(taskIndex)
-mc.submitTask(taskIndex,taskBasis)
+taskBasis = qsti.queryTaskResult(taskIndex)
+mc.submitTask(taskIndex, taskBasis)
 # %% [markdown]
 # # 查询结果
 
 mc.queryCheck(taskIndex)
 mc.queryTask(taskIndex)
-
